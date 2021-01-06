e94bbb0ede5d9e7edc11c82d4254a65e20dd89b1
Robin Hilliard <robin@rocketboots.com>
2021-01-06 20:15:53 +1100

    Add test suite for Note and fixes

--- lib/xnote/core/note.ex
+++ lib/xnote/core/note.ex
@@ -2,34 +2,37 @@ defmodule XNote.Core.Note do
   @moduledoc """
   Note state snapshot
   """
+  alias XNote.Core.Tag
+  alias XNote.Core.Para
   
   
   defstruct ~w[id tags paras]a
   @type t :: %__MODULE__{
                id: non_neg_integer(),
-               tags: MapSet.t(XNote.Core.Tag),
-               paras: [XNote.Core.Para]}
+               tags: MapSet.t(Tag),
+               paras: [Para]}
   
   
-  @spec new(non_neg_integer | nil) :: %__MODULE__{}
+  @spec new(non_neg_integer) :: %__MODULE__{}
+  def new(), do: new(Id.new())
   def new(id) do
     %__MODULE__{
-      id: Id.new(id),
+      id: id,
       tags: MapSet.new(),
       paras: []
     }
   end
   
   
-  @spec add_tag(%__MODULE__{}, XNote.Core.Tag) :: %__MODULE__{}
+  @spec add_tag(%__MODULE__{}, Tag) :: %__MODULE__{}
   def add_tag(note, tag), do: %{note | tags: MapSet.put(note.tags, tag)}
   
   
-  @spec remove_tag(%__MODULE__{}, XNote.Core.Tag) :: %__MODULE__{}
+  @spec remove_tag(%__MODULE__{}, Tag) :: %__MODULE__{}
   def remove_tag(note, tag), do: %{note | tags: MapSet.delete(note.tags, tag)}
   
   
-  @spec insert_para(%__MODULE__{}, XNote.Core.Para, non_neg_integer()) :: %__MODULE__{}
+  @spec insert_para(%__MODULE__{}, Para, non_neg_integer()) :: %__MODULE__{}
   def insert_para(note, para, index), do: %{note | paras: List.insert_at(note.paras, index, para)}
   
   
@@ -41,7 +44,7 @@ defmodule XNote.Core.Note do
   def swap_paras(note,index_a, index_b) do
     {min_index, max_index} = if index_a < index_b, do: {index_a, index_b}, else: {index_b, index_a}
     {para, _paras} = List.pop_at(note.paras, max_index)
-    insert_para(note, para, min_index)
+    remove_para(note, max_index) |> insert_para(para, min_index)
   end
 
 end
--- test/note_test.exs
+++ test/note_test.exs
@@ -1,5 +1,74 @@
 defmodule NoteTest do
   use ExUnit.Case
   alias XNote.Core.Note
+  alias XNote.Core.Tag
+  alias XNote.Core.Para
+  
+  test "new note" do
+    assert %Note{
+      id: _,
+      tags: %MapSet{},
+      paras: []
+    } = Note.new()
+  end
+  
+  test "add a tag" do
+    tag = Tag.new("Nothing")
+    tags = MapSet.new([tag])
+    assert %Note{
+      id: _,
+      tags: ^tags,
+      paras: []
+    } = Note.new()
+        |> Note.add_tag(tag)
+  end
+  
+  test "remove a tag" do
+    tag_a = Tag.new("A")
+    tag_b = Tag.new("B")
+    tags = MapSet.new([tag_b])
+    assert %Note{
+      id: _,
+      tags: ^tags,
+      paras: []
+    } = Note.new()
+        |> Note.add_tag(tag_a)
+        |> Note.add_tag(tag_b)
+        |> Note.remove_tag(tag_a)
+  end
+  
+  test "insert a paragraph" do
+    para = Para.new() |> Para.insert_text("All alone")
+    assert %Note{
+      id: _,
+      tags: %MapSet{},
+      paras: [para]
+    } = Note.new()
+      |> Note.insert_para(para, 0)
+  end
+
+  test "remove a paragraph" do
+    para = Para.new() |> Para.insert_text("All alone")
+    assert %Note{
+      id: _,
+      tags: %MapSet{},
+      paras: []
+    } = Note.new()
+      |> Note.insert_para(para, 0)
+      |> Note.remove_para(0)
+  end
+  
+  test "swap paragraphs" do
+    para_a = Para.new() |> Para.insert_text("A")
+    para_b = Para.new() |> Para.insert_text("B")
+    assert %Note{
+      id: _,
+      tags: %MapSet{},
+      paras: [para_b, para_a]
+    } = Note.new()
+      |> Note.insert_para(para_a, 0)
+      |> Note.insert_para(para_b, 1)
+      |> Note.swap_paras(0, 1)
+  end
   
 end
